76ac6b79bb76cae94b85d166c5823c81ea27e8bf
Max <sudovsky@cloudunion.ru>
2025-05-26 03:29:06 +0300

    Can save/loda data to/from file

--- Sources/Extensions/Data.swift
+++ Sources/Extensions/Data.swift
@@ -37,4 +37,18 @@ public extension Data {
         
     }
 
+    func saveDataFile(withName name: String) throws {
+        let tempDirectoryURLs = FileManager.default.urls(for: .applicationDirectory, in: .userDomainMask)
+        let tempDirectoryURL = tempDirectoryURLs.first!
+        let fileURL = tempDirectoryURL.appendingPathComponent(name)
+        try self.write(to: fileURL)
+    }
+    
+    func loadDataFile(withName name: String) throws -> Data {
+        let tempDirectoryURLs = FileManager.default.urls(for: .applicationDirectory, in: .userDomainMask)
+        let tempDirectoryURL = tempDirectoryURLs.first!
+        let fileURL = tempDirectoryURL.appendingPathComponent(name)
+        return try Data(contentsOf: fileURL)
+    }
+    
 }
