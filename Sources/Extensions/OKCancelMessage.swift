042da758d439964a296af045cc11bd8fcec8b11b
Max <sudovsky@cloudunion.ru>
2026-05-10 01:07:02 +0300

    okCancelMessage revert

--- Sources/Extensions/OKCancelMessage.swift
+++ Sources/Extensions/OKCancelMessage.swift
@@ -20,15 +20,15 @@ public struct OKCancelMessage: ViewModifier {
     var okTitle = "OK"
     var cancelTitle = "Cancel"
 
-    var onOk: ((Any?) -> Void)? = nil
-    var onCancel: ((Any?) -> Void)? = nil
+    var onOk: (() -> Void)? = nil
+    var onCancel: (() -> Void)? = nil
 
     public func body(content: Content) -> some View {
         return AnyView(content
             .alert(title, isPresented: $showingAlert) {
                 Button(okTitle, action: submit)
                 if showCancel {
-                    Button(cancelTitle, role: .cancel) { onCancel?(<#Any?#>) }
+                    Button(cancelTitle, role: .cancel) { onCancel?() }
                 }
             } message: { if subtitle == nil { EmptyView() } else { Text(subtitle ?? "") } }
             .tint(tintColor)
@@ -37,7 +37,7 @@ public struct OKCancelMessage: ViewModifier {
         func submit() {
             showingAlert = false
             Task {
-                onOk?(<#Any?#>)
+                onOk?()
             }
         }
     }
@@ -45,11 +45,11 @@ public struct OKCancelMessage: ViewModifier {
 
 public extension View {
     
-    func okCancelMessage(showingAlert: Binding<Bool>, title: Binding<String>, subtitle: Binding<String?>? = nil, onOk: ((Any?) -> Void)? = nil, onCancel: ((Any?) -> Void)? = nil) -> some View {
+    func okCancelMessage(showingAlert: Binding<Bool>, title: Binding<String>, subtitle: Binding<String?>? = nil, onOk: (() -> Void)? = nil, onCancel: (() -> Void)? = nil) -> some View {
         modifier(OKCancelMessage(showCancel: true, showingAlert: showingAlert, title: title, subtitle: subtitle ?? .constant(nil), onOk: onOk))
     }
     
-    func okMessage(showingAlert: Binding<Bool>, title: Binding<String>, subtitle: Binding<String?>? = nil, onOk: ((Any?) -> Void)? = nil) -> some View {
+    func okMessage(showingAlert: Binding<Bool>, title: Binding<String>, subtitle: Binding<String?>? = nil, onOk: (() -> Void)? = nil) -> some View {
         modifier(OKCancelMessage(showCancel: false, showingAlert: showingAlert, title: title, subtitle: subtitle ?? .constant(nil), onOk: onOk))
     }
     
