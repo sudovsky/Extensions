65023ff1632a6b1ef1e24a3239c7abbb1a19e992
Max <sudovsky@cloudunion.ru>
2026-05-10 00:02:18 +0300

    OKCancelMessage added

---
+++ Sources/Extensions/OKCancelMessage.swift
@@ -0,0 +1,56 @@
+//
+//  OKCancelMessage.swift
+//  Extensions
+//
+//  Created by Max Sudovsky on 08.05.2026.
+//
+
+import SwiftUI
+
+struct OKCancelMessage: ViewModifier {
+    
+    var showCancel: Bool
+    @Binding var showingAlert: Bool
+    
+    @Binding var title: String
+    @Binding var subtitle: String?
+    
+    var tintColor: Color? = .accentColor
+    
+    var okTitle = "OK"
+    var cancelTitle = "Cancel"
+
+    var onOk: (() -> Void)? = nil
+    var onCancel: (() -> Void)? = nil
+
+    func body(content: Content) -> some View {
+        return AnyView(content
+            .alert(title, isPresented: $showingAlert) {
+                Button(okTitle, action: submit)
+                if showCancel {
+                    Button(cancelTitle, role: .cancel) { onCancel?() }
+                }
+            } message: { if subtitle == nil { EmptyView() } else { Text(subtitle ?? "") } }
+            .tint(tintColor)
+        )
+        
+        func submit() {
+            showingAlert = false
+            Task {
+                onOk?()
+            }
+        }
+    }
+}
+
+extension View {
+    
+    func okCancelMessage(showingAlert: Binding<Bool>, title: Binding<String>, subtitle: Binding<String?>? = nil, onOk: (() -> Void)? = nil) -> some View {
+        modifier(OKCancelMessage(showCancel: true, showingAlert: showingAlert, title: title, subtitle: subtitle ?? .constant(nil), onOk: onOk))
+    }
+    
+    func okMessage(showingAlert: Binding<Bool>, title: Binding<String>, subtitle: Binding<String?>? = nil, onOk: (() -> Void)? = nil) -> some View {
+        modifier(OKCancelMessage(showCancel: false, showingAlert: showingAlert, title: title, subtitle: subtitle ?? .constant(nil), onOk: onOk))
+    }
+    
+}
